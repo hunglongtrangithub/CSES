3f265ab31ac4b7f14408da7aa64b796222af7b47
hunglongtrangithub <hunglongtran2004@gmail.com>
2023-01-14 14:29:28 -0500

    Added comments for the Factory Machines problem

--- Sorting and Searching/FactoryMachines.cpp
+++ Sorting and Searching/FactoryMachines.cpp
@@ -7,17 +7,25 @@ int main() {
     ll n, t;
     cin >> n >> t;
     vector<ll> machines(n); 
-    for (ll i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> machines[i];
     }
-    // binary search 
-    ll left = 0, right = 1e18;
+    /* We have an array of the maximum number of items produced max_product according to the given time as the index 
+    (time ranges from 0 to 10^18). Because max_product increases with time, we can use binary search to find 
+    the minimum time required to produce t items by finding the smallest index where its max_product >= t. */ 
+    ll left = 0, right = 1e18; 
     while (left < right) {
         ll mid = (left + right) / 2;
         ll max_product = 0; // max number of items produced in time mid
+        /* The upper bound for max_product is (2 * 10^5) * (10^18 / 1) = 2 * 10^23, which is larger than 
+        the maximum value of ll (2^63 - 1). So we need to prevent overflow by stopping adding up max_product 
+        when it is already >= t. */
         for (int i = 0; i < n; i++) {
-            max_product += min(mid / machines[i], (ll)1e9);
+            max_product += mid / machines[i];
+            if (max_product >= t) 
+                break; // no need to continue if max_product is already >= t
         }
+
         if (max_product >= t) {
             right = mid;
         } else {
---
+++ Sorting and Searching/ReadingBooks.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#define ll long long
+using namespace std;
+
+int main() {
+    
+}
