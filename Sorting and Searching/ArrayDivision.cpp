9ea257b763a5daf0cee7f7459510ce47fe0eb67e
hunglongtrangithub <hunglongtran2004@gmail.com>
2023-02-02 19:53:46 -0500

    Solution of the Array Division problem

---
+++ Sorting and Searching/ArrayDivision.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main() {
+	long n, k;
+	cin >> n >> k;
+	vector<long> x(n);
+	long max_element = INT32_MIN; // the maximum element in the array
+	for (long i = 0; i < n; i++) {
+	    cin >> x[i];
+		max_element = max(max_element, x[i]);
+	}
+    // The subarray sum is at least the maximum element in the array, and at most the sum of all elements in the array.
+	long left = max_element; 
+	long right = 2 * long(10e14); // (2 * 10^5) * 10^9 = 2 * 10^14
+    /* For a number in the range [left, right], we check if we are able to split the array into k subarrays whose sums are 
+    all less than or equal to the number, by finding how many subarrays we need to split the array into so that each 
+    subarray sum is less than or equal to that number. If the count is greater than k, it means the number is too small to 
+    satisfy the condition, so we increase the number and check again. Eventually, we will find the smallest number that 
+    can satisfy the condition, after which all other larger numbers will also satisfy the condition. 
+    We can apply binary search on the range [left, right] to find the smallest number that can satisfy the condition. 
+    Because there exists an optimal division where the largest subarray sum is minimized, the number we have found through
+    binary search is the minimum largest subarray sum, which is the answer.
+    */
+	while (left < right) {
+        long mid = (left + right) / 2; // the number we are checking
+        long count = 1; // the number of subarrays we need to split the array into
+        long sum = 0; // the sum of the current subarray
+        for (long num : x) {
+            // if the sum of the current subarray plus the next number is greater than mid, we need to split the array
+            if (sum + num > mid) { 
+                count++;
+                sum = 0;
+            }
+            sum += num;
+        }
+        if (count > k) {
+            left = mid + 1; // ignore numbers smaller than or equal to mid
+        } else {
+            right = mid; // ignore numbers larger than mid
+        }
+	}
+    // the search stops when left == right, which is the answer
+	cout << left;
+  return 0;
+}
--- Sorting and Searching/FactoryMachines.cpp
+++ Sorting and Searching/FactoryMachines.cpp
@@ -9,10 +9,16 @@ int main() {
     for (long i = 0; i < n; i++) {
         cin >> machines[i];
     }
-    /* We have an array of the maximum number of items produced max_product according to the given time as the index 
-    (time ranges from 0 to 10^18). Because max_product increases with time, we can use binary search to find 
-    the minimum time required to produce t items by finding the smalongest index where its max_product >= t. */ 
-    long left = 0, right = long(1e18); 
+    long left = 0;
+    long right = long(1e18); // 10^18 = 1 * 10^9 * 10^9 (worst case: 1 machine, 10^9 items, 10^9 time)
+    /*  For a given time in the range [left, right], we check if we are able to produce t items in that time, by finding
+    maxmimally how many items we can produce in that time. If the number of items produced is smaller than t, it means the 
+    time is too small to satisfy the condition, so we increase the time and check again. Eventually, we will find the 
+    smallest time that can satisfy the condition, after which all other larger times will also satisfy the condition.
+    We can apply binary search on the range [left, right] to find the smallest time that can satisfy the condition.
+    Because there exists an optimal production plan where the time taken is minimized, the time we have found through 
+    binary search is the minimum time, which is the answer.
+    */ 
     while (left < right) {
         long mid = (left + right) / 2;
         long max_product = 0; // max number of items produced in time mid
@@ -22,15 +28,16 @@ int main() {
         for (long i = 0; i < n; i++) {
             max_product += mid / machines[i];
             if (max_product >= t) {
-                break; // no need to continue if max_product is already >= t
+                break; 
             }
         }
         if (max_product >= t) {
-            right = mid;
+            right = mid; // ignore numbers larger than mid
         } else {
-            left = mid + 1;
+            left = mid + 1; // ignore numbers smaller than or equal to mid
         }
     }
+    // the search stops when left == right, which is the answer
     cout << left;
     return 0;
 }
